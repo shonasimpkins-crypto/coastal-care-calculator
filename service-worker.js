ffa7d96c55319d5e5b6cad9d6f8a1a1db9455103
shonasimpkins-crypto <shona.simpkins@gmail.com>
2026-07-12 13:58:04 +1000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("carecalc-v1").then(cache => {
+      return cache.addAll([
+        "./calculator.html",
+        "./manifest.json",
+        "./service-worker.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
